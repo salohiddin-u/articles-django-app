cedd2417f399edfc2e3e02bf2d68cde5c388340f
salohiddin-u <saloh.usmonali@gmail.com>
2025-11-02 23:18:40 +0500

    update

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from .models import *
+
 # Create your views here.
+def index(request):
+    data = {
+        "articles": Article.objects.all(),
+    }
+    return render(request, 'index.html', data)
+
+def article(request, a):
+    data = {
+        "article": Article.objects.get(id=a),
+    }
+    return render(request, 'article.html', data)
